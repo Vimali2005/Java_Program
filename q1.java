c4979d7aa9d7e4e83812d560d35fe0303194308c
Vimali.K <v981693139@gmail.com>
2026-06-21 11:30:05 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,11 @@
+class q1{
+    public static void main(String[]args){
+        int Age = 26;
+        String Name = "Kavi";
+
+        System.out.println("My Name is "+ Name);
+        System.out.println("My Age is " + Age);
+
+    }
+
+}
